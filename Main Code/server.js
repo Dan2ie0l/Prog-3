323d415113bac0ae9b8975ce0d496c064275b235
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2020-01-31 17:10:45 +0400

    hhhhh

--- Main Code/server.js
+++
@@ -1,8 +0,0 @@
-var grassArr = require("/Modules/class.grass.js");
-var xotakerner = require("/Modules/class.xotaker.js");
-var gishatichner = require("/Modules/class.gishatich.js");
-var vorsord = require("/Modules/class.vorsord.js");
-var mahh = require("/Modules/class.mah.js");
-
-
-
